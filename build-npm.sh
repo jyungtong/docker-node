a4e6c1977d9db0510f6209a4ec08732b0c0c7b32
Tux Tong <huntthetux@gmail.com>
2015-10-10 09:41:01 +0800

    added npm builder

---
+++ build-npm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export NODE_VERSION=v4.1.1
+export NODE_PATH=/opt/runtime/node-${NODE_VERSION}
+export PATH=${NODE_PATH}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+apk --update add gcc g++ python make && \
+cd /tmp/ && npm install && \
+mv /tmp/node_modules/* /opt/app/node_modules/ && \
+apk del gcc g++ python && \
+rm -rf /tmp/node_modules /var/cache/apk/*
