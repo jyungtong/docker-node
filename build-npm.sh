15aae98950475cb98c33e02da2aa2b06503799f4
Tux Tong <huntthetux@gmail.com>
2015-10-18 11:10:16 +0800

    fixed should build node on shared host dir

--- build-npm.sh
+++ build-npm.sh
@@ -4,8 +4,7 @@ export NODE_VERSION=v4.2.1
 export NODE_PATH=/opt/runtime/node-${NODE_VERSION}
 export PATH=${NODE_PATH}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-apk --update add gcc g++ python make && \
-cd /tmp/ && npm install && \
-mv /tmp/node_modules/* /opt/app/node_modules/ && \
-apk del gcc g++ python && \
-rm -rf /tmp/node_modules /var/cache/apk/*
+apk --update add gcc g++ python make libgcc libstdc++ && \
+cd /opt/app && npm install && \
+apk del gcc g++ python make libgcc libstdc++ && \
+rm -rf /tmp/* /var/cache/apk/*
--- compile-node.sh
+++ compile-node.sh
@@ -3,11 +3,12 @@
 export NODE_VERSION=v4.2.1
 export NODE_PATH=/opt/runtime/node-${NODE_VERSION}
 
+echo "http://repos.lax-noc.com/alpine/edge/main" > /etc/apk/repositories && \
 apk add --update curl make gcc g++ python linux-headers paxctl libgcc libstdc++ && \
 cd /tmp && mkdir -p ${NODE_PATH} && \
 curl -sSL https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}.tar.gz | tar -xz && \
 cd node-${NODE_VERSION} && \
-./configure --prefix=${NODE_PATH} ${CONFIG_FLAGS} && \
+./configure --prefix=${NODE_PATH} && \
 make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1) && \
 make install && \
 paxctl -cm ${NODE_PATH}/bin/node && \
@@ -16,8 +17,8 @@ if [ -x ${NODE_PATH}/bin/npm ]; then \
   ${NODE_PATH}/bin/npm install -g npm && \
   find ${NODE_PATH}/lib/node_modules/npm -name test -o -name .bin -type d | xargs rm -rf; \
 fi && \
-apk del curl make gcc g++ python linux-headers paxctl ${DEL_PKGS} && \
+apk del curl make gcc g++ python linux-headers paxctl libgcc libstdc++ && \
 rm -rf /etc/ssl /tmp/node-${NODE_VERSION} ${RM_DIRS} \
-${NODE_PATH}/share/man /tmp/* /var/cache/apk/* /root/.npm /root/.node-gyp \
-${NODE_PATH}/lib/node_modules/npm/man ${NODE_PATH}/lib/node_modules/npm/doc \
-${NODE_PATH}/lib/node_modules/npm/html
+    ${NODE_PATH}/share/man /tmp/* /var/cache/apk/* /root/.npm /root/.node-gyp \
+    ${NODE_PATH}/lib/node_modules/npm/man ${NODE_PATH}/lib/node_modules/npm/doc \
+    ${NODE_PATH}/lib/node_modules/npm/html
